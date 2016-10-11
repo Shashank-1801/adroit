d3b570ea23ebb9781c1e1f8d41475b666dd02dc2
Shashank Shekhar <shashankeshav@gmail.com>
2016-10-10 23:31:21 -0700

    added few more functions

--- homework3.py
+++ homework3.py
@@ -1,27 +1,28 @@
 value_mat = []
 board_mat = []
 n = 0
-play_symbol = ".";
+play_symbol = ".";  #will get changed
+opponent = ".";     #will get changed
 neutral = ".";
-opponent = ".";
 
 #MIN_MAX functions
-def minmax(val_list, depth=0):
-    if depth == 0:
-        max_val(val_list);
-    else:
-        #not sure what to do here!!
-        minmax(list, depth-1);
+def minmax(move_list, depth, board_state):
+    #get the possible moves that can be made
+    #for each of these move calculate the value
+    #find the max among these values
+    max_val(move_list, depth, board_state)
 
     print("TO-DO");
 
 
-def min_val(val_list, depth):
+def min_val(move_list, depth):
+    #return the min of all the values given
     print("Will return the min value");
 
 
-def max_val(val_list, depth):
-    index = val_list.index()
+def max_val(move_list, depth, board_state):
+    #return the max of the values given
+    index = move_list.index()
     print("Will return the max value")
 
 
@@ -29,10 +30,26 @@ def max_val(val_list, depth):
 def alphabeta():
     print("TO-DO");
 
+def get_val_list(pos_list):
+    vals_for_pos_list = [];
+    for move in pos_list:
+        vals_for_pos_list.append(get_pos_value(move));
+    return vals_for_pos_list;
 
 def get_pos_value(pos):
     global opponent;
     global n;
+    x,y = pos_to_index(pos);
+    total_val = value_mat[x][y];
+    neighbours = get_neighbours(pos);
+    for s in neighbours:
+        #print(index_to_pos(s[0],s[1]), value_mat[s[0]][s[1]]);
+        if(board_mat[s[0]][s[1]] == opponent):
+            total_val += value_mat[s[0]][s[1]];
+    return total_val;
+
+
+def get_neighbours(pos):
     x, y = pos_to_index(pos);
     neighbours = [];
     if x + 1 < n:
@@ -43,13 +60,7 @@ def get_pos_value(pos):
         neighbours.append([x - 1, y]);
     if y - 1 >= 0:
         neighbours.append([x, y - 1]);
-    total_val = 0;
-    print(neighbours)
-    for s in neighbours:
-        print(index_to_pos(s[0],s[1]), value_mat[s[0]][s[1]]);
-        if(board_mat[s[0]][s[1]] == "." or board_mat[s[0]][s[1]] == opponent):
-            total_val += value_mat[s[0]][s[1]];
-    return total_val;
+    return neighbours;
 
 
 #finds the possible places for next move
@@ -65,16 +76,16 @@ def find_possible_moves():
     return next_possible_moves;
 
 
-def pos_to_index(pos):
+def pos_to_index(pos):      #C2 => x=1, y=2
     p = list(pos);
-    x = ord(p[0]) - ord("A");
-    y = int(pos[1])-1;
+    y = ord(p[0]) - ord("A");
+    x = int(pos[1])-1;
     return x,y;
 
 
-def index_to_pos(x, y):
+def index_to_pos(x, y):     #x=1 => 2  , y=2 => C
     offset = ord("A");
-    pos = str(chr(offset + x)) + str(y + 1);
+    pos = str(chr(offset + y)) + str(x + 1);
     return pos;
 
 
@@ -158,15 +169,33 @@ def read_input(file):
     fwrt = open("output.txt", 'w');
     fwrt.write(line);
 
+def update_board(best_move, board_state):
+    x,y = pos_to_index(best_move);
+    board_mat[x][y] = play_symbol;
+    neighbours = get_neighbours(best_move);
+    for s in neighbours:
+        # print(index_to_pos(s[0],s[1]), value_mat[s[0]][s[1]]);
+        if (board_mat[s[0]][s[1]] == opponent):
+            board_mat[s[0]][s[1]] = play_symbol;
 
+
+
+#start of main method
 read_input("input.txt");
-pos = find_possible_moves();
+possible_moves = find_possible_moves();
+best_move = minmax(possible_moves);
+print("best move is " , best_move);
+print(get_pos_value(best_move));
+update_board(best_move);
+get_board_state();
+"""
 vals = [];
-for p in pos:
+for p in possible_moves:
     print(p);
     vals.append(get_pos_value(p));
     ind = vals.index(max(vals));
     max_val = max(vals);
     print(max_val, " is the max value and at index ", ind, " out of ", len(vals), " positions");
     print();
+"""
 print("Done!");
